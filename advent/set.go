c6118d53b1636033eb145cbbf2d62f322b571b60
Michael McClimon <michael@mcclimon.org>
2024-11-18 19:27:28 -0500

    advent: add basic set type
    This is basically an excuse to poke around with the new Go
    range-over-func feature.

---
+++ advent/set.go
@@ -0,0 +1,34 @@
+package advent
+
+import (
+	"iter"
+)
+
+type Set[T comparable] map[T]struct{}
+
+var unit struct{}
+
+func NewSet[T comparable](elems ...T) Set[T] {
+	s := make(map[T]struct{}, len(elems))
+	for _, elem := range elems {
+		s[elem] = unit
+	}
+
+	return Set[T](s)
+}
+
+func (s Set[T]) Add(elems ...T) {
+	for _, elem := range elems {
+		s[elem] = unit
+	}
+}
+
+func (s Set[T]) Iter() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for v := range s {
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
