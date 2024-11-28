164b7db0d9be18ee8a9011454751d9b117ca1a38
Michael McClimon <michael@mcclimon.org>
2024-11-28 16:17:00 -0500

    input: make it more ergonomic
    (And less go-like.)

--- advent/input/input.go
+++ advent/input/input.go
@@ -4,12 +4,31 @@ import (
 	"bufio"
 	"io"
 	"iter"
+	"os"
 	"strconv"
 )
 
+type Input struct {
+	r io.Reader
+}
+
+// Get an io.Reader for the first command-line arg; defaulting to stdin.
+func New() *Input {
+	if len(os.Args) == 1 {
+		return &Input{os.Stdin}
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	return &Input{f}
+}
+
 // NB throws away errors.
-func Lines(r io.Reader) iter.Seq[string] {
-	scanner := bufio.NewScanner(r)
+func (i *Input) Lines() iter.Seq[string] {
+	scanner := bufio.NewScanner(i.r)
 	return func(yield func(string) bool) {
 		for scanner.Scan() {
 			yield(scanner.Text())
@@ -17,20 +36,20 @@ func Lines(r io.Reader) iter.Seq[string] {
 	}
 }
 
-func Ints(r io.Reader) iter.Seq[int] {
+func (i *Input) Ints() iter.Seq[int] {
 	return func(yield func(int) bool) {
-		for line := range Lines(r) {
+		for line := range i.Lines() {
 			n, _ := strconv.Atoi(line)
 			yield(n)
 		}
 	}
 }
 
-func Hunks(r io.Reader) iter.Seq[[]string] {
+func (i *Input) Hunks() iter.Seq[[]string] {
 	var buf []string
 
 	return func(yield func([]string) bool) {
-		for line := range Lines(r) {
+		for line := range i.Lines() {
 			if line == "" {
 				yield(buf)
 				buf = nil
