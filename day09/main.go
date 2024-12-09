a9c9e07932b120369e3ed7c44cb1785c0e903185
Michael McClimon <michael@mcclimon.org>
2024-12-09 09:07:01 -0500

    day 9: part 2, ugly solution
    Not pictured: 20 minutes looking for a stupid bug.

--- day09/main.go
+++ day09/main.go
@@ -14,7 +14,7 @@ const Empty = -1
 func main() {
 	in := input.New().Slurp()
 
-	disk := make([]int, 0, len(in)*5)
+	disk1 := make([]int, 0, len(in)*5)
 	isFile := true
 	id := 0
 
@@ -26,9 +26,9 @@ func main() {
 		n := conv.Atoi(string(char))
 		for range n {
 			if isFile {
-				disk = append(disk, id)
+				disk1 = append(disk1, id)
 			} else {
-				disk = append(disk, Empty)
+				disk1 = append(disk1, Empty)
 			}
 		}
 
@@ -38,11 +38,14 @@ func main() {
 		isFile = !isFile
 	}
 
-	part1(disk)
+	disk2 := make([]int, len(disk1))
+	copy(disk2, disk1)
+
+	part1(disk1)
+	part2(disk2, id-1)
 }
 
 func part1(disk []int) {
-	// printDisk(disk)
 	lp := slices.Index(disk, Empty)
 	rp := lastFull(disk)
 
@@ -57,19 +60,7 @@ func part1(disk []int) {
 		rp = lastFull(disk)
 	}
 
-	// printDisk(disk)
-	// take one more pass: we could compute this as we go, but also meh
-	checksum := 0
-	for i, n := range disk {
-		if n == Empty {
-			break
-		}
-		// fmt.Printf("%d * %d\n", i, n)
-
-		checksum += i * n
-	}
-
-	fmt.Println(checksum)
+	fmt.Println("part 1:", checksum(disk))
 }
 
 func lastFull(disk []int) int {
@@ -79,6 +70,81 @@ func lastFull(disk []int) int {
 		}
 	}
 
+	return -1
+}
+
+func checksum(disk []int) int {
+	checksum := 0
+	for i, n := range disk {
+		if n == Empty {
+			continue
+		}
+
+		checksum += i * n
+	}
+
+	return checksum
+}
+
+func part2(disk []int, startID int) {
+	for id := startID; id >= 0; id-- {
+		// loop
+		start, length := startEndForFull(disk, id)
+		free := indexForFree(disk, length)
+
+		if free == -1 || free >= start {
+			continue
+		}
+
+		// fmt.Printf("moving id %d (len=%d) from %d to %d\n", id, length, start, free)
+
+		for i := range length {
+			disk[free+i] = id
+			disk[start+i] = Empty
+		}
+
+	}
+
+	fmt.Println("part 2:", checksum(disk))
+}
+
+func startEndForFull(disk []int, id int) (int, int) {
+	start := slices.Index(disk, id)
+	length := 0
+	for i := start; i < len(disk) && disk[i] == id; i++ {
+		length++
+	}
+
+	return start, length
+	// fmt.Println(start, length)
+}
+
+func indexForFree(disk []int, length int) int {
+	if length >= 10 {
+		panic("length too big?")
+	}
+
+	for i := 0; i < len(disk); i++ {
+		if disk[i] != Empty {
+			continue
+		}
+
+		start := i
+		// fmt.Println("looking for space of", length, "starting at", i)
+
+		for j := i; j < len(disk) && disk[j] == Empty; j++ {
+			// fmt.Println("checking", j, "at i", i)
+			if 1+j-start >= length {
+				return start
+			}
+
+			i++
+		}
+
+		// i = start
+	}
+
+	// panic("no free space found")
 	return -1
 }
 
