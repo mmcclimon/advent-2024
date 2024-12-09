3d5ecd27828d244690a278a1c49472de260672f0
Michael McClimon <michael@mcclimon.org>
2024-12-09 18:05:25 -0500

    day 9: silly, refactor and speed up
    There's no reason to do this other than I was bored.

--- day09/main.go
+++ day09/main.go
@@ -7,69 +7,120 @@ import (
 
 	"github.com/mmcclimon/advent-2024/advent/conv"
 	"github.com/mmcclimon/advent-2024/advent/input"
+	"github.com/mmcclimon/advent-2024/advent/operator"
 )
+
+type Disk struct {
+	s             []int
+	maxID         int
+	firstEmptyIdx int
+	lastFullIdx   int
+	// only used in part 2
+	ids        map[int]blockData
+	freeBlocks []*blockData
+}
+
+type blockData struct{ idx, length int }
 
 const Empty = -1
 
 func main() {
 	in := strings.TrimSpace(input.New().Slurp())
 
-	disk1 := make([]int, 0, len(in)*5)
-	isFile := true
-	id := 0
+	part1(NewDisk(in))
+	part2(NewDisk(in))
+}
 
-	for _, char := range in {
+func NewDisk(line string) *Disk {
+	s := make([]int, 0, len(line)*5)
+	isFile := true
+
+	var id, lfi int
+	var freeBlocks []*blockData
+	ids := make(map[int]blockData, len(line))
+
+	for _, char := range line {
+		toAppend := operator.CrummyTernary(isFile, id, Empty)
+		startIdx := len(s)
 		n := conv.Atoi(string(char))
-		toAppend := Empty
-		if isFile {
-			toAppend = id
-		}
+		block := blockData{startIdx, n}
 
 		for range n {
-			disk1 = append(disk1, toAppend)
+			s = append(s, toAppend)
 		}
 
 		if isFile {
+			lfi = len(s) - 1
+			ids[id] = block
 			id++
+		} else {
+			freeBlocks = append(freeBlocks, &block)
 		}
+
 		isFile = !isFile
 	}
 
-	disk2 := make([]int, len(disk1))
-	copy(disk2, disk1)
-
-	part1(disk1)
-	part2(disk2, id-1)
+	return &Disk{
+		s:             s,
+		maxID:         id - 1,
+		firstEmptyIdx: slices.Index(s, Empty), // will be fast: always <= 10
+		lastFullIdx:   lfi,
+		ids:           ids,
+		freeBlocks:    freeBlocks,
+	}
 }
 
-func part1(disk []int) {
-	lp := slices.Index(disk, Empty)
-	rp := lastFullIndex(disk)
+func part1(disk *Disk) {
+	lp := disk.firstEmptyIdx
+	rp := disk.lastFullIdx
 
 	for lp < rp {
-		disk[lp] = disk[rp]
-		disk[rp] = Empty
+		disk.swap(lp, rp)
 
-		lp = slices.Index(disk, Empty)
-		rp = lastFullIndex(disk)
+		lp = disk.firstEmptyIdx
+		rp = disk.lastFullIdx
 	}
 
-	fmt.Println("part 1:", checksum(disk))
+	fmt.Println("part 1:", disk.Checksum())
 }
 
-func lastFullIndex(disk []int) int {
-	for i := len(disk) - 1; i >= 0; i-- {
-		if disk[i] != Empty {
-			return i
+func (d *Disk) swap(left, right int) {
+	d.s[left] = d.s[right]
+	d.s[right] = Empty
+
+	// recompute indexes
+	for i := d.lastFullIdx; i >= 0; i-- {
+		if d.s[i] != Empty {
+			d.lastFullIdx = i
+			break
 		}
 	}
 
-	return -1
+	for i := d.firstEmptyIdx; i < len(d.s); i++ {
+		if d.s[i] == Empty {
+			d.firstEmptyIdx = i
+			break
+		}
+	}
 }
 
-func checksum(disk []int) int {
+func (d *Disk) swapBlocks(free *blockData, id blockData, freeIdx int) {
+	for i := range id.length {
+		d.swap(free.idx+i, id.idx+i)
+	}
+
+	if free.length == id.length {
+		d.freeBlocks = slices.Delete(d.freeBlocks, freeIdx, freeIdx+1)
+		return
+	}
+
+	free.length -= id.length
+	free.idx += id.length
+}
+
+func (d *Disk) Checksum() int {
 	checksum := 0
-	for i, n := range disk {
+	for i, n := range d.s {
 		if n == Empty {
 			continue
 		}
@@ -80,52 +131,31 @@ func checksum(disk []int) int {
 	return checksum
 }
 
-func part2(disk []int, startID int) {
-	for id := startID; id >= 0; id-- {
-		start, length := startAndLengthForID(disk, id)
-		idx := findFreeBlock(disk, length)
+func part2(disk *Disk) {
+	for id := disk.maxID; id >= 0; id-- {
+		data := disk.dataForID(id)
+		i, free := disk.findFreeBlock(data.length)
 
-		if idx == -1 || idx >= start {
+		if free == nil || free.idx >= data.idx {
 			continue
 		}
 
-		for i := range length {
-			disk[idx+i] = id
-			disk[start+i] = Empty
-		}
-
+		disk.swapBlocks(free, data, i)
 	}
 
-	fmt.Println("part 2:", checksum(disk))
+	fmt.Println("part 2:", disk.Checksum())
 }
 
-func startAndLengthForID(disk []int, id int) (int, int) {
-	start := slices.Index(disk, id)
-
-	length := 0
-	for i := start; i < len(disk) && disk[i] == id; i++ {
-		length++
-	}
-
-	return start, length
+func (d *Disk) dataForID(id int) blockData {
+	return d.ids[id]
 }
 
-func findFreeBlock(disk []int, length int) int {
-	for i := 0; i < len(disk); i++ {
-		if disk[i] != Empty {
-			continue
-		}
-
-		start := i
-
-		for j := i; j < len(disk) && disk[j] == Empty; j++ {
-			if 1+j-start >= length {
-				return start
-			}
-
-			i++
+func (d *Disk) findFreeBlock(length int) (int, *blockData) {
+	for i, block := range d.freeBlocks {
+		if block.length >= length {
+			return i, block
 		}
 	}
 
-	return -1
+	return -1, nil
 }
