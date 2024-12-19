7326a26690f23c00f7fe47bc2d143fbc528ae6cc
Michael McClimon <michael@mcclimon.org>
2024-12-19 18:52:58 -0500

    day 19: add part 2 solution
    This was cribbed pretty heavily from hints on reddit, but I think it's a
    reasonable way to go. My first pass was going to take a skrillion years
    to run.

--- day19/main.go
+++ day19/main.go
@@ -15,12 +15,38 @@ func main() {
 	towels := strings.Split(hunks[0][0], ", ")
 	re := regexp.MustCompile("^(?:" + strings.Join(towels, "|") + ")+$")
 
-	total := 0
+	p1, p2 := 0, 0
 	for _, line := range hunks[1] {
-		if re.MatchString(line) {
-			total++
+		if !re.MatchString(line) {
+			continue
 		}
+
+		p1++
+		p2 += try(towels, line)
 	}
 
-	fmt.Println(total)
+	fmt.Println("part 1:", p1)
+	fmt.Println("part 2:", p2)
+}
+
+func try(towels []string, line string) int {
+	res := []int{1}
+
+	var waysHere int
+	for i := 1; i <= len(line); i++ {
+		waysHere = 0
+
+		for _, towel := range towels {
+			// If we're looking at "bbrbw", and towel "bw", and there are three
+			// different ways to get to "bbr", then there are also 3 different ways
+			// to get to "bbrbw".
+			if strings.HasSuffix(line[:i], towel) {
+				waysHere += res[i-len(towel)]
+			}
+		}
+
+		res = append(res, waysHere)
+	}
+
+	return waysHere
 }
