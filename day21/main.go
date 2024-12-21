e0d0df0f090ab0ee3fcc11b0d152313809fe985a
Michael McClimon <michael@mcclimon.org>
2024-12-21 14:31:39 -0500

    day 21: refactor part 1 with structs
    I'm going to rewrite all of this anyway, I suspect, but I wanted to
    clean it up before I did.

--- day21/main.go
+++ day21/main.go
@@ -11,15 +11,26 @@ import (
 	"github.com/mmcclimon/advent-2024/advent/collections"
 )
 
-var test = []string{"029A", "980A", "179A", "456A", "379A"}
-var actual = []string{"169A", "279A", "540A", "869A", "789A"}
+type Keypad struct {
+	coords    map[rune]xy
+	coordsRev collections.Set[xy]
+	memo      map[memoKey][]string
+}
 
-// right: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-// mine:  <v<A>A<A>>^AvAA<^A>A<v<A>>^AvA^A<vA^>A<v<A>^A>AAvA^A<v<A>A^>AAA<A>vA^A
+type xy struct{ x, y int }
+
+type memoKey struct{ from, to rune }
+
+var (
+	test = []string{"029A", "980A", "179A", "456A", "379A"}
+	//nolint:unused
+	actual = []string{"169A", "279A", "540A", "869A", "789A"}
+)
 
 func main() {
-	strs := actual
+	strs := test
 	total := 0
+
 	for _, want := range strs {
 		dirs := inputString(want)
 
@@ -37,16 +48,6 @@ func main() {
 	}
 
 	fmt.Println(total)
-
-	/*
-		r1 := inputDirections(dirs)
-		fmt.Println(r1)
-
-		r2 := inputDirections(r1)
-		fmt.Println(r2)
-
-		fmt.Println(len(r2))
-	*/
 }
 
 func inputString(want string) []string {
@@ -54,7 +55,7 @@ func inputString(want string) []string {
 	runes := []rune("A" + want)
 
 	for i := range len(runes) - 1 {
-		allDirs := directionsForNumeric(runes[i], runes[i+1])
+		allDirs := numeric.directionsFor(runes[i], runes[i+1])
 
 		if len(all) == 0 {
 			all = allDirs
@@ -85,24 +86,11 @@ func findDirections(want []string) []string {
 }
 
 func inputDirections(want string) []string {
-	/*
-		var dirs string
-		runes := []rune("A" + want)
-
-		for i := range len(runes) - 1 {
-			dirs += directionsForDirectional(runes[i], runes[i+1])
-		}
-
-		// fmt.Println(dirs)
-
-		return dirs
-	*/
-
 	var all []string
 	runes := []rune("A" + want)
 
 	for i := range len(runes) - 1 {
-		allDirs := directionsForDirectional(runes[i], runes[i+1])
+		allDirs := directional.directionsFor(runes[i], runes[i+1])
 
 		if len(all) == 0 {
 			all = allDirs
@@ -134,10 +122,7 @@ func inputDirections(want string) []string {
     +---+---+
 */
 
-type xy struct{ x, y int }
-type memoKey struct{ from, to rune }
-
-var numeric = map[rune]xy{
+var numeric = NewKeypad(map[rune]xy{
 	'7': {0, 0},
 	'8': {1, 0},
 	'9': {2, 0},
@@ -149,25 +134,61 @@ var numeric = map[rune]xy{
 	'3': {2, 2},
 	'0': {1, 3},
 	'A': {2, 3},
+})
+
+/*
++---+---+---+
+|   | ^ | A |
++---+---+---+
+| < | v | > |
++---+---+---+
+*/
+var directional = NewKeypad(map[rune]xy{
+	'^': {1, 0},
+	'A': {2, 0},
+	'<': {0, 1},
+	'v': {1, 1},
+	'>': {2, 1},
+})
+
+func dirFor(from, to xy) string {
+	switch {
+	case to == xy{from.x - 1, from.y}:
+		return "<"
+	case to == xy{from.x + 1, from.y}:
+		return ">"
+	case to == xy{from.x, from.y - 1}:
+		return "^"
+	case to == xy{from.x, from.y + 1}:
+		return "v"
+	default:
+		panic("unreachable")
+	}
 }
 
-var numMemo = make(map[memoKey][]string)
+func NewKeypad(coords map[rune]xy) *Keypad {
+	return &Keypad{
+		coords:    coords,
+		coordsRev: collections.NewSet(slices.Collect(maps.Values(coords))...),
+		memo:      make(map[memoKey][]string),
+	}
+}
 
-func directionsForNumeric(from, to rune) []string {
+func (k *Keypad) directionsFor(from, to rune) []string {
 	key := memoKey{from, to}
-	if cached, ok := numMemo[key]; ok {
+	if cached, ok := k.memo[key]; ok {
 		return cached
 	}
 
 	// another day, another dijkstra
-	dist := make(map[xy]int, len(numeric))
-	prev := make(map[xy][]xy, len(numeric))
+	dist := make(map[xy]int, len(k.coords))
+	prev := make(map[xy][]xy, len(k.coords))
 
 	q := collections.NewMinQueue(func(a, b xy) int {
 		return cmp.Compare(dist[a], dist[b])
 	})
 
-	start := numeric[from]
+	start := k.coords[from]
 
 	dist[start] = 0
 	q.Insert(start)
@@ -175,12 +196,12 @@ func directionsForNumeric(from, to rune) []string {
 	for q.Len() > 0 {
 		cur := q.ExtractMin()
 
-		if numeric[to] == cur {
+		if k.coords[to] == cur {
 			// fmt.Println("found", string(to), dist[cur])
 			break
 		}
 
-		for _, v := range neighbors(numeric, cur) {
+		for _, v := range k.neighbors(cur) {
 			alt := dist[cur] + 1
 			existingDist, ok := dist[v]
 
@@ -201,7 +222,7 @@ func directionsForNumeric(from, to rune) []string {
 		}
 	}
 
-	paths := findPaths(prev, numeric[from], numeric[to])
+	paths := findPaths(prev, k.coords[from], k.coords[to])
 	all := collections.NewSet[string]()
 
 	for _, path := range paths {
@@ -214,7 +235,24 @@ func directionsForNumeric(from, to rune) []string {
 	}
 
 	ret := slices.Collect(all.Iter())
-	numMemo[key] = ret
+	k.memo[key] = ret
+	return ret
+}
+
+func (k *Keypad) neighbors(pos xy) []xy {
+	ret := make([]xy, 0, 4)
+
+	for _, candidate := range []xy{
+		{pos.x - 1, pos.y},
+		{pos.x + 1, pos.y},
+		{pos.x, pos.y - 1},
+		{pos.x, pos.y + 1},
+	} {
+		if k.coordsRev.Contains(candidate) {
+			ret = append(ret, candidate)
+		}
+	}
+
 	return ret
 }
 
@@ -232,151 +270,18 @@ func findPaths(prev map[xy][]xy, start, end xy) [][]xy {
 		pos := path[len(path)-1]
 
 		if pos == start {
-			// fmt.Println("found path", path)
 			slices.Reverse(path)
 			paths = append(paths, path)
-			// seen.Add(pos)
 			continue
 		}
 
-		// if !seen.Contains(pos) {
 		for _, coords := range prev[pos] {
 			next := make([]xy, len(path)+1)
 			copy(next, path)
 			next[len(path)] = coords
 			s.Append(next)
 		}
-		// }
 	}
 
 	return paths
 }
-
-/*
-+---+---+---+
-|   | ^ | A |
-+---+---+---+
-| < | v | > |
-+---+---+---+
-*/
-var directional = map[rune]xy{
-	'^': {1, 0},
-	'A': {2, 0},
-	'<': {0, 1},
-	'v': {1, 1},
-	'>': {2, 1},
-}
-
-var dirMemo = make(map[memoKey][]string)
-
-func directionsForDirectional(from, to rune) []string {
-	key := memoKey{from, to}
-	if cached, ok := dirMemo[key]; ok {
-		return cached
-	}
-
-	// another day, another dijkstra
-	dist := make(map[xy]int, len(directional))
-	prev := make(map[xy][]xy, len(directional))
-
-	q := collections.NewMinQueue(func(a, b xy) int {
-		return cmp.Compare(dist[a], dist[b])
-	})
-
-	start := directional[from]
-
-	dist[start] = 0
-	q.Insert(start)
-
-	for q.Len() > 0 {
-		cur := q.ExtractMin()
-
-		if directional[to] == cur {
-			// fmt.Println("found", string(to), dist[cur])
-			break
-		}
-
-		for _, v := range neighbors(directional, cur) {
-			alt := dist[cur] + 1
-			existingDist, ok := dist[v]
-
-			if ok && alt > existingDist {
-				continue
-			}
-
-			dist[v] = alt
-			q.Insert(v)
-
-			// If this is equal to the one we already know about, add it to the
-			// list; if it's better (or we don't have one at all), make a new list.
-			if alt == existingDist {
-				prev[v] = append(prev[v], cur)
-			} else {
-				prev[v] = []xy{cur}
-			}
-		}
-	}
-
-	paths := findPaths(prev, directional[from], directional[to])
-	all := collections.NewSet[string]()
-
-	for _, path := range paths {
-		var dirs string
-		for i := range len(path) - 1 {
-			dirs += dirFor(path[i], path[i+1])
-		}
-
-		all.Add(dirs + "A")
-	}
-
-	ret := slices.Collect(all.Iter())
-	numMemo[key] = ret
-	return ret
-}
-
-func neighbors(grid map[rune]xy, pos xy) []xy {
-	ret := make([]xy, 0, 4)
-	coords := slices.Collect(maps.Values(grid))
-
-	for _, candidate := range []xy{
-		{pos.x - 1, pos.y},
-		{pos.x + 1, pos.y},
-		{pos.x, pos.y - 1},
-		{pos.x, pos.y + 1},
-	} {
-		if slices.Contains(coords, candidate) {
-			ret = append(ret, candidate)
-		}
-	}
-
-	return ret
-}
-
-func dirFor(from, to xy) string {
-	switch {
-	case to == xy{from.x - 1, from.y}:
-		return "<"
-	case to == xy{from.x + 1, from.y}:
-		return ">"
-	case to == xy{from.x, from.y - 1}:
-		return "^"
-	case to == xy{from.x, from.y + 1}:
-		return "v"
-	default:
-		panic("unreachable")
-	}
-}
-
-/*
-right: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-mine:  <v<A>A<A>>^AvAA<^A>A<v<A>>^AvA^A<vA^>A<v<A>^A>AAvA^A<v<A>A^>AAA<A>vA^A
-
-right: v<<A>>^A<A>AvA<^AA>A<vAAA>^A
-mine:  <v<A>>^A<A>AvA<^AA>A<vAAA^>A
-
-right: <A^A>^^AvvvA
-mine:  <A^A>^^AvvvA
-
-right: 029A
-
-*/
