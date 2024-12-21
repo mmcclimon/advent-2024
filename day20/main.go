099737b35f6ababbca73e41e4c296b87345e3c51
Michael McClimon <michael@mcclimon.org>
2024-12-20 22:23:57 -0500

    day 20: part 2 solution
    This was more involved than I thought it would be, and I have one stpuid
    bug, but I don't care that much about finding it.

--- day20/main.go
+++ day20/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
+	"maps"
+	"slices"
 
+	"github.com/mmcclimon/advent-2024/advent/collections"
 	"github.com/mmcclimon/advent-2024/advent/input"
+	"github.com/mmcclimon/advent-2024/advent/mathx"
 )
 
 type rc struct{ r, c int }
@@ -22,7 +27,8 @@ func main() {
 	}
 
 	dist := makeDist(grid, start)
-	findShortcuts(grid, dist)
+	part1(grid, dist)
+	part2(grid, dist)
 }
 
 func makeDist(grid map[rc]rune, start rc) map[rc]int {
@@ -32,19 +38,10 @@ func makeDist(grid map[rc]rune, start rc) map[rc]int {
 	pos := start
 
 	for grid[pos] != 'E' {
-		for _, candidate := range []rc{
-			{pos.r - 1, pos.c},
-			{pos.r + 1, pos.c},
-			{pos.r, pos.c - 1},
-			{pos.r, pos.c + 1},
-		} {
-
-			next, ok := grid[candidate]
-			if !ok || next == '#' {
-				continue
-			}
-
-			if dist[candidate] > 0 {
+		for _, candidate := range neighbors(grid, pos) {
+			next := grid[candidate]
+			_, ok := dist[candidate]
+			if next == '#' || ok {
 				continue
 			}
 
@@ -57,7 +54,7 @@ func makeDist(grid map[rc]rune, start rc) map[rc]int {
 	return dist
 }
 
-func findShortcuts(grid map[rc]rune, dist map[rc]int) {
+func part1(grid map[rc]rune, dist map[rc]int) {
 	shortcuts := make(map[int]int)
 
 	for pos, thisDist := range dist {
@@ -94,4 +91,92 @@ func findShortcuts(grid map[rc]rune, dist map[rc]int) {
 	}
 
 	fmt.Println("part 1:", total)
+}
+
+func part2(grid map[rc]rune, path map[rc]int) {
+	shortcuts := make(map[int]int)
+
+	for pos, thisDist := range path {
+		if grid[pos] == 'E' {
+			continue
+		}
+
+		dist := make(map[rc]int, len(grid))
+
+		q := collections.NewMinQueue(func(a, b rc) int {
+			return cmp.Compare(dist[a], dist[b])
+		})
+
+		dist[pos] = 0
+		q.Insert(pos)
+
+		for q.Len() > 0 {
+			cur := q.ExtractMin()
+
+			if grid[cur] == 'E' {
+				continue
+			}
+
+			// We do 25 here because the dijkstra distance isn't exactly the thing
+			// we want (apparently).
+			for _, v := range neighbors(grid, cur) {
+				alt := dist[cur] + 1
+				_, haveDist := dist[v]
+				if alt <= 25 && (!haveDist || alt < dist[v]) {
+					dist[v] = alt
+					q.Insert(v)
+				}
+			}
+		}
+
+		for finish := range dist {
+			haveDist, onPath := path[finish]
+			if !onPath {
+				continue
+			}
+
+			// I don't know why the taxicab distance is sometimes not the same as
+			// the dijkstra gistance, but the taxicab math makes the example work
+			// out correctly.
+			newDist := mathx.Abs(pos.r-finish.r) + mathx.Abs(pos.c-finish.c)
+			if newDist > 20 {
+				continue
+			}
+
+			alt := thisDist + newDist
+
+			if alt < haveDist {
+				shortcuts[haveDist-alt]++
+			}
+		}
+	}
+
+	total := 0
+	for _, k := range slices.Sorted(maps.Keys(shortcuts)) {
+		if k < 100 {
+			continue
+		}
+
+		total += shortcuts[k]
+	}
+
+	fmt.Println("part 2:", total)
+}
+
+func neighbors(grid map[rc]rune, pos rc) []rc {
+	ret := make([]rc, 0, 4)
+
+	for _, candidate := range []rc{
+		{pos.r - 1, pos.c},
+		{pos.r + 1, pos.c},
+		{pos.r, pos.c - 1},
+		{pos.r, pos.c + 1},
+	} {
+		_, ok := grid[candidate]
+		if ok {
+			ret = append(ret, candidate)
+		}
+	}
+
+	return ret
 }
